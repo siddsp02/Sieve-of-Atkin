b9b7c466cdff049022b39fdaf42c9e3b945c740c
Sidd <35938702+siddsp02@users.noreply.github.com>
2022-03-17 00:04:26 -0700

    Removed bounds for better performance.
    Instead of having multiple bound ranges, the bounds were removed completely. In the end, performance became worse for smaller input values, but improved significantly for larger inputs.

--- sieve_of_atkin.py
+++ sieve_of_atkin.py
@@ -1,31 +1,30 @@
 # !usr/bin/env python3
 
 """An implementation of the Sieve of Atkin algorithm in Python.
-How the algorithm works can be read in the references below. 
+How the algorithm works can be read in the references below.
 
 References:
     - https://en.wikipedia.org/wiki/Sieve_of_Atkin
-
-Note: Iteration for the algorithm is currently very slow due
-to the range iterators not having the optimized limits,
-wasting lots of loop iterations.
 """
 
 import cProfile
 import doctest
-import pstats
-from collections import defaultdict
 from itertools import product
-from math import isqrt
+import math
+import pstats
 
 
-def sieve_of_atkin(limit: int) -> list[int]:
+def square(x: int) -> int:
+    return x*x
+
+
+def sieve_of_atkin(limit: int):
     """Returns the number of primes from 2 to a specified limit in a list.
-
+    
     An algorithm for finding all primes less than or equal to the limit
     specified to the function. The algorithm works by not only marking
     multiples of a number, but also multiples of its square, and checking
-    modulus 60 remainders.
+    modulo 60 remainders.
 
     :param limit: The maximum value for a prime number.
     :type limit: int
@@ -49,48 +48,26 @@ def sieve_of_atkin(limit: int) -> list[int]:
     >>> len(sieve_of_atkin(200_000))
     17984
     """
-
-    wheel = {1, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 49, 53, 59}
-    sieve, lim = defaultdict(bool), isqrt(limit) + 1
-    xset, yset = range(1, isqrt(limit // 4) + 1), range(1, lim, 2)
-
-    for x, y in product(xset, yset):
-        n = 4*x*x + y*y
-        if n <= limit and n % 60 in {1, 13, 17, 29, 37, 41, 49, 53}:
+    sieve = [False] * limit
+    root = math.isqrt(limit) + 1
+    squares = map(square, range(root))
+    for x2, y2 in product(squares, repeat=2):
+        n = 4*x2 + y2
+        if n < limit and n % 60 in {1, 13, 17, 29, 37, 41, 49, 53}:
             sieve[n] ^= True
-
-    xset, yset = range(1, isqrt(limit // 3) + 1), range(2, lim, 2)
-
-    for x, y in product(xset, yset):
-        n = 3*x*x + y*y
-        if n <= limit and n % 60 in {7, 19, 31, 43}:
+        n = 3*x2 + y2
+        if n < limit and n % 60 in {7, 19, 31, 43}:
             sieve[n] ^= True
-
-    lim = isqrt(limit * 2 + 3) // 2 + 1
-
-    for x in range(2, lim):
-        for y in range(1, x):
-            n = 3*x*x - y*y
-            if n <= limit and n % 60 in {11, 23, 47, 59}:
+        if x2 > y2:
+            n = 3*x2 - y2
+            if n < limit and n % 60 in {11, 23, 47, 59}:
                 sieve[n] ^= True
-
-    wset, nwset = range(0, limit, 60), range(0, limit // 49, 60)
-
-    for w, x in product(wset, wheel):
-        npow = (n := w+x) * n
-        if npow < limit and sieve[n]:
-            for w, x in product(nwset, wheel):
-                if (c := npow * (w+x)) <= limit:
-                    sieve[c] = False
-
     results = [2, 3, 5]
-
-    for w, x in product(wset, wheel):
-        if (n := w+x) > limit:
-            break
-        if sieve[n]:
-            results.append(n)
-
+    for i, prime in enumerate(sieve):
+        if i > 1 and prime:
+            results.append(i)
+            for x in range(i*i, limit, i*i):
+                sieve[x] = False
     return results
 
 
@@ -101,5 +78,5 @@ def main(n: int = 10_000) -> None:
 
 
 if __name__ == "__main__":
-    # doctest.testmod()
-    main(250000)
+    doctest.testmod()
+    main(250_000)
